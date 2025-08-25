4b9d60dbb1959973cd89c667597bf5761c83e48f
Wout Vereijken <91586@roc-teraa.nl>
2025-08-25 11:39:14 +0200

    Add project files.

---
+++ Quizmester/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Quizmester;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
---
+++ Quizmester/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System.Windows;
+
+namespace Quizmester
+{
+    public partial class MainWindow : Window
+    {
+        private enum CurrentScreen
+        {
+            WelcomeScreen,
+            LoginScreen,
+            CreateAccountScreen
+        }
+
+        private void ShowScreen(CurrentScreen screen)
+        {
+            // Hide all screens first
+            WelcomeScreen.Visibility = Visibility.Collapsed;
+            LoginScreen.Visibility = Visibility.Collapsed;
+            CreateAccountScreen.Visibility = Visibility.Collapsed;
+
+            // Show selected screen
+            switch (screen)
+            {
+                case CurrentScreen.WelcomeScreen:
+                    WelcomeScreen.Visibility = Visibility.Visible;
+                    break;
+                case CurrentScreen.LoginScreen:
+                    LoginScreen.Visibility = Visibility.Visible;
+                    break;
+                case CurrentScreen.CreateAccountScreen:
+                    CreateAccountScreen.Visibility = Visibility.Visible;
+                    break;
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ShowScreen(CurrentScreen.WelcomeScreen);
+        }
+
+        private void LoginButton(object sender, RoutedEventArgs e)
+        {
+            ShowScreen(CurrentScreen.LoginScreen);
+        }
+        private void BackButton(object sender, RoutedEventArgs e)
+        {
+            ShowScreen(CurrentScreen.WelcomeScreen);
+        }
+        private void CreateAccountButton(object sender, RoutedEventArgs e)
+        {
+            ShowScreen(CurrentScreen.CreateAccountScreen);
+        }
+    }
+}
